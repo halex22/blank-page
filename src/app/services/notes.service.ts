add896623b2a71e8c9e4c35585d05885a98ac3be
halex22 <halex2209@gmail.com>
2025-04-10 13:23:17 +0200

    first note works

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { type Note } from '../models/note';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
 
-  constructor() { }
+  private readonly STORAGE_KEY = 'notes'
+
+  note: Note
+
+  constructor() { 
+    this.note = this.loadNote()
+  }
+
+  saveNote(oldNote: Note): void {
+    oldNote.last_edit = Date.now()
+    localStorage.setItem(this.STORAGE_KEY, JSON.stringify(oldNote))
+  }
+
+  loadNote(): Note {
+    const savedNote = localStorage.getItem(this.STORAGE_KEY)
+    return savedNote ? JSON.parse(savedNote) : this.generateDefaultNote()
+  }
+
+  generateDefaultNote(): Note {
+    const defaultNote: Note = {
+      content: 'sample content',
+      creation_date: Date.now(),
+      last_edit: Date.now()
+    }
+    return defaultNote
+  }
+
 }
