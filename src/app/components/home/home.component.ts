4dcad7a4bd2f7fa16726bfa16dba537956fc8b51
halex22 <halex2209@gmail.com>
2025-05-01 22:33:51 +0200

    remove page component and display note after view init

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,27 +1,38 @@
-import { Component, computed, inject, output, signal } from '@angular/core';
-import { PageContentComponent } from '../page-content/page-content.component';
+import { Component, computed, inject} from '@angular/core';
 import { SaveOnLeaveDirective } from '../../directives/save-on-leave.directive';
 import { DetailsComponent } from '../details/details.component';
 import { NotesService } from '../../services/notes.service';
 import { UpdateContentDirective } from '../../directives/update-content.directive';
 import { HeaderComponent } from '../header/header.component';
 import { DrawerComponent } from "../drawer/drawer.component";
+import { ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  imports: [PageContentComponent, SaveOnLeaveDirective, DetailsComponent, UpdateContentDirective, HeaderComponent, DrawerComponent],
+  imports: [ SaveOnLeaveDirective, DetailsComponent, UpdateContentDirective, HeaderComponent, DrawerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
   service = inject(NotesService)
-
   totalWords = computed(() => this.service.currentNote().content.match(/\w+/g)?.length ?? 0)
   totalChars = computed(() => this.service.currentNote().content.length)
 
+  @ViewChild('editable') editableRef!: ElementRef<HTMLElement>
+
   constructor() {
     setInterval(() => this.service.saveAllNotes(), 5000)
+  }
+
+  ngAfterViewInit() {
+    const note = this.service.currentNote()
+    const html = note.content.replace(/\n/g, '<br>')
+    this.editableRef.nativeElement.innerHTML = html
+  }
+
+  get formattedContent () {
+    return this.service.currentNote().content.replace(/\n/g, '<br>')
   }
  
   saveChanges = () => { 
